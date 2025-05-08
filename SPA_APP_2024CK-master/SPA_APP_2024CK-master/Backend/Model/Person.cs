25a2d3ba886000b6fafebcc7bd15bf9a5198511b
Christoferkrabbi <160104752+Christoferkrabbi@users.noreply.github.com>
2025-05-08 08:55:41 +0300

    Delete SPA_APP_2024CK-master directory

--- SPA_APP_2024CK-master/SPA_APP_2024CK-master/Backend/Model/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Model
-{
-    public record Person
-    {
-        public int Id { get; set; }
-        public int? Age { get; set; }
-        public string? Name { get; set; }     
-    }
-}
--- SPA_APP_2024CK-master/SPA_APP_2024CK-master/frontend/src/views/HomeView.vue
+++
@@ -1,3 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>Nigga ma vihkan seda</template>
